46c07abf888e791dd3cd9fbe021d9b5e9d3af833
allhearingeye <all.hearing.eye@gmail.com>
2022-04-11 22:45:51 +0400

    fixed an error when tmux is not running

--- linux-install.sh
+++ linux-install.sh
@@ -3,19 +3,28 @@
 vimplug=$HOME/.vim/autoload/plug.vim
 undodir=$HOME/.vim/undodir
 
-if [ -f "$vimplug" ]; then
+if [[ -f "$vimplug" ]]; then
     echo "Vim-plug exists"
 else
     echo "Downloading vim-plug..."
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || exit
 fi
 
-[ -d $undodir ] || mkdir -p $undodir
+[[ -d $undodir ]] || mkdir -p $undodir
 echo "Installing..."
-[ -f "$HOME/.vimrc" ] && rm $HOME/.vimrc
+[[ -f "$HOME/.vimrc" ]] && rm $HOME/.vimrc
 ln -s $PWD/.vimrc $HOME/.vimrc
-vim +'PlugInstall --sync' +qa
-[ -f "$HOME/.tmux.conf" ] && rm $HOME/.tmux.conf
+vim +"PlugInstall --sync" +qa
+[[ -f "$HOME/.tmux.conf" ]] && rm $HOME/.tmux.conf
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
-tmux source-file $HOME/.tmux.conf
-echo "Complete!"
+
+if [[ -n $TMUX ]]; then
+    tmux source-file $HOME/.tmux.conf
+    read -n1 -p "You need to restart tmux to fix colors in vim. Kill tmux? [y/n]" doit
+    case $doit in
+      y|Y) tmux kill-server;;
+      *  ) echo -e  "\n\nDon't forget to restart tmux manually";;
+    esac
+fi
+
+echo "Installation complete!"
