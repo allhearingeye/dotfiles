5843056f95cdc190164a4f106cc267f470da71bb
allhearingeye <all.hearing.eye@gmail.com>
2022-11-05 23:41:24 +0400

    added russian keyboard layout switcher

--- win-install.ps1
+++ win-install.ps1
@@ -1,36 +1,38 @@
-$VimPlug = $HOME + '/vimfiles/autoload/plug.vim'
-$UndoDir = $HOME + '/vimfiles/undodir'
+# $VimPlug = $HOME + '/vimfiles/autoload/plug.vim'
+# $UndoDir = $HOME + '/vimfiles/undodir'
+#
+#
+# if (-not(Test-Path -Path $VimPlug -PathType Leaf))
+# {
+#     try
+#     {
+#         Write-Host 'Downloading vim-plug...'
+#         Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | New-Item $VimPlug -Force | Out-Null
+#         Write-Host 'Download complete!'
+#     } catch
+#     {
+#         $_.Exception
+#         Exit
+#     }
+# } else
+# {
+#     Write-Host 'Vim-plug exists'
+# }
+#
+# if (-not(Test-Path -Path $UndoDir))
+# {
+#     New-Item -ItemType 'directory' -Path $UndoDir | Out-Null
+# }
+#
+# Write-Host 'Installing...'
+#
+# if (Test-Path -Path $HOME/*vimrc)
+# {
+#     Remove-Item $HOME/*vimrc
+# }
+#
+# New-Item -Path $HOME/.vimrc -ItemType HardLink -Value .vimrc | Out-Null
+# vim +'PlugInstall --sync' +qa
+# Write-Host 'Installation complete!'
 
-
-if (-not(Test-Path -Path $VimPlug -PathType Leaf))
-{
-    try
-    {
-        Write-Host 'Downloading vim-plug...'
-        Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | New-Item $VimPlug -Force | Out-Null
-        Write-Host 'Download complete!'
-    } catch
-    {
-        $_.Exception
-        Exit
-    }
-} else
-{
-    Write-Host 'Vim-plug exists'
-}
-
-if (-not(Test-Path -Path $UndoDir))
-{
-    New-Item -ItemType 'directory' -Path $UndoDir | Out-Null
-}
-
-Write-Host 'Installing...'
-
-if (Test-Path -Path $HOME/*vimrc)
-{
-    Remove-Item $HOME/*vimrc
-}
-
-New-Item -Path $HOME/.vimrc -ItemType HardLink -Value .vimrc | Out-Null
-vim +'PlugInstall --sync' +qa
-Write-Host 'Installation complete!'
+Copy-Item -Force -Recurse nvim $env:LOCALAPPDATA\
