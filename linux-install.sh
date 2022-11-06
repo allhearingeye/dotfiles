c2fd7b763735d3bf99c92ee531c827389123ddd2
Kali <all.hearing.eye@gmail.com>
2022-11-06 22:28:55 +0400

    linux install fix

--- linux-install.sh
+++ linux-install.sh
@@ -3,6 +3,7 @@
 vimplug=$HOME/.vim/autoload/plug.vim
 undodir=$HOME/.vim/undodir
 
+# vim install
 if [[ -f "$vimplug" ]]; then
     echo "Vim-plug exists"
 else
@@ -10,35 +11,13 @@ else
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || exit
 fi
 
-# Set display for WSL
-if [[ $(uname -a | grep [Mm]icrosoft) ]]; then
-    display="export DISPLAY=\"\$(/sbin/ip route | awk '/default/ { print \$3 }'):0\""
-
-    if [[ -n "`$SHELL -c 'echo $ZSH_VERSION;'`" ]]; then
-        shellrc="$HOME/.zshrc"
-    elif [[ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]]; then
-        shellrc="$HOME/.bashrc"
-    else
-        echo ".bashrc and .zshrc not found"
-    fi
-
-    if [[ -n $shellrc ]]; then
-        case `grep -Fx "$display" "$shellrc" >/dev/null; echo $?` in
-            0) ;;
-            1)
-              echo -e "$display" >> $shellrc
-              ;;
-            *)
-              echo "Something went wrong while set DISPLAY"
-            ;;
-        esac
-    fi
-fi
-
 [[ -d $undodir ]] || mkdir -p $undodir
 echo "Installing..."
 cp .vimrc $HOME/.vimrc
+echo -e "For xkbswitch run: \nsudo cp ./utils/libxkbswitch.so /usr/local/lib/libxkbswitch.so"
+vim +"PlugInstall --sync" +qa
 
+# tmux install
 cp .tmux.conf $HOME/.tmux.conf
 
 if [[ -n $TMUX ]]; then
@@ -50,5 +29,6 @@ if [[ -n $TMUX ]]; then
     esac
 fi
 
+# Other config install
 cp -R $PWD/.config $HOME/
 
