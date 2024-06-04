e68046c836132e3d4e232e0ce4ad279c97eb7a06
allhearingeye <all.hearing.eye@gmail.com>
2024-06-04 23:01:12 +0400

    added tab rename key binding

--- .wezterm.lua
+++ .wezterm.lua
@@ -63,6 +63,18 @@ config.keys = {
   { key = 'p', mods = 'CTRL', action = action.SendKey { key = 'UpArrow', mods = 'NONE' } },
   { key = 'n', mods = 'CTRL', action = action.SendKey { key = 'DownArrow', mods = 'NONE' } },
   { key = 'd', mods = 'CTRL', action = action.CloseCurrentPane { confirm = true } },
+  { key = ',', mods = 'LEADER', action = action.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
 }
 
 config.key_tables = {
